4c9e7dfbb1b505bec06573658af960e1fb8f42c2
Barak Eitan <barakeitan48@gmail.com>
2023-04-29 17:41:48 +0300

    add telemetry api handle

---
+++ controllers/telemetry.js
@@ -0,0 +1,40 @@
+const http = require("http");
+
+exports.get_all_telemetry = (req, res) => {
+    const options = {
+        hostname: "127.0.0.1",
+        port: 8001,
+        method: "GET"
+    };
+
+    let res_data="";
+
+    const request = http.request(options, api_res =>{
+        let data = '';
+        api_res.on("data", d => {
+            console.log(d);
+            data = d;
+        });
+
+        api_res.on("error", error =>{
+            console.log({"error":error});
+        });
+    
+        api_res.on("end", () =>{
+            try {
+                res_data = JSON.parse(data);
+                process.stdout.write(data);
+                res.status(200).json(res_data);
+            } catch (error) {
+                res.status(401).write(error);
+            }
+        });
+    });
+
+    request.end();
+
+    request.on('finish', () => {
+        console.log("on finish");
+        console.log(res_data);
+    });
+};

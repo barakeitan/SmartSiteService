3d8e60a92b2d06a9efc15e8cb2fd6c90db780ab1
elad aviv <elad.aviv@workiz.com>
2023-05-24 16:03:10 +0300

    create malfunction modals

---
+++ src/models/malfunction.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const malfunctionSchema = new Schema({
+    roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+    sensorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Sensor' },
+    date: Date.now(),
+    recent_data: String,
+    message: String, //a message from the server or from the raspberry pie
+});
+const Malfunction = mongoose.model("Malfunction", malfunctionSchema);
+module.exports = Malfunction;

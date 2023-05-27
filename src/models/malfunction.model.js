b20cb851008fb38ce05482c3e92c3df7de99f09c
elad aviv <elad.aviv@workiz.com>
2023-05-28 01:44:37 +0300

    fix models

--- src/models/malfunction.model.js
+++ src/models/malfunction.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const malfunctionSchema = new Schema({
     roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
     sensorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Sensor' },
+    malfunctionTypeId: { type: mongoose.Schema.Types.ObjectId, ref: 'MalfunctionType' },
     date: Date.now(),
     recent_data: String,
     message: String, //a message from the server or from the raspberry pie

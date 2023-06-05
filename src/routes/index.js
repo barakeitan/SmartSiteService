5b2ff4662446b204cea384e913cddef62fb1081e
shirbalas <Sbalas@attentigroup.com>
2023-06-05 21:04:24 +0300

    [FIX]: Add site controller and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const RoomRouter = require('./room.routes');
+const SiteRouter = require('./site.routes');
 const sensorRouter = require('./sensor.routes');
 const malfunctionRouter = require('./malfunction.routes');
 const recordRouter = require('./record.routes');
 
 router.use('/room',RoomRouter);
+router.use('/site',SiteRouter);
 router.use('/sensor',sensorRouter);
 router.use('/malfunction',malfunctionRouter);
 router.use('/record',recordRouter);

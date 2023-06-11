b186832f14e0c306a7cc952250959e555965e25d
elad aviv <elad.aviv@workiz.com>
2023-06-11 22:14:03 +0300

    fix passing param by roomId

--- src/controllers/malfunction.controller.js
+++ src/controllers/malfunction.controller.js
@@ -2,7 +2,7 @@ const Malfunction = require('../models/malfunction.model');
 
 exports.getMalfunctionsByRoomId = async (req, res) => {
     try {
-        const malfunctions = await Malfunction.find({ _id: req.params.roomId }).populate("sensorId", "malfunctionTypeId").exec();
+        const malfunctions = await Malfunction.find({ roomId: req.params.roomId }).populate("sensorId", "malfunctionTypeId").exec();
         res.status(200).json(malfunctions);
     } catch (error) {
         console.log(error);

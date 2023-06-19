23ac5bb7eaf7d44b0465c29223c8782da1c5e6bd
Barak Eitan <barakeitan48@gmail.com>
2023-06-19 15:08:02 +0300

    populate the malfunction id in the getMalfunction

--- src/controllers/malfunction.controller.js
+++ src/controllers/malfunction.controller.js
@@ -2,18 +2,21 @@ const Malfunction = require('../models/malfunction.model');
 
 exports.getMalfunctionsByRoomId = async (req, res) => {
     try {
-        const malfunctions = await Malfunction.find({ roomId: req.params.roomId })
+        const malfunctions = await Malfunction.find({ roomId: req.params.roomId,  
+            malfunctionTypeId: { $ne: null }, severity: {$ne: null},
+            sensorId : {$ne : null} })
         .populate({
             path: 'sensorId',
-            select: 'sensorTypeId',
             populate: {
-            path: 'sensorTypeId',
-            model: 'SensorType',
-            },
-        })
-        .limit(100)
+              path: 'sensorTypeId',
+              model: 'SensorType'
+            }
+          })
+        .populate('malfunctionTypeId')
         .sort([['date', -1]])
+        .limit(100)
         .exec();
+        console.log("malfunctions:"+malfunctions)
         res.status(200).json(malfunctions);
     } catch (error) {
         console.log(error);

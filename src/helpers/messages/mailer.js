1067da2252dde036f017833d63bfce1b94844440
elad aviv <elad.aviv@workiz.com>
2023-06-19 23:20:13 +0300

    change syntax in send mail

--- src/helpers/messages/mailer.js
+++ src/helpers/messages/mailer.js
@@ -20,8 +20,8 @@ async function main() {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
+    from: 'smartSiteAltert@gmail.com', // sender address
+    to: "e.aviv23@gmail.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
